f3e440fa799276ac35b3a69f6437e4296ba40bd9
Vladimir <vladimir.kosmovsky@yandex.ru>
2023-10-08 00:28:43 +0300

    Update scripts.js
    New JS functions

--- scripts.js
+++ scripts.js
@@ -1,3 +1,28 @@
 function copyToClipboard () {
     navigator.clipboard.writeText('play.wintertale.su');
 }
+
+function toMainPage() {
+    document.getElementById("right-conteiner-main-content").style.display = 'flex';
+    document.getElementById("right-container-rules-content").style.display = 'none';
+    document.getElementById("right-container-loyal-content").style.display = 'none';
+    alert("nice");
+}
+
+function toRulesPage() {
+    document.getElementById("right-conteiner-main-content").style.display = 'none';
+    document.getElementById("right-container-rules-content").style.display = 'flex';
+    document.getElementById("right-container-loyal-content").style.display = 'none';
+    alert("nice");
+}
+
+function toLoyalPage() {
+    document.getElementById("right-conteiner-main-content").style.display = 'none';
+    document.getElementById("right-container-rules-content").style.display = 'none';
+    document.getElementById("right-container-loyal-content").style.display = 'flex';
+    alert("nice");
+}
+
+function redirect(link) {
+    window.open(link, '_blank').focus();
+}
